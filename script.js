c0a6560a264d60f802966a3683dc3cf17c71285d
Ayuba Abdullahi Jr <ayubaabdullahijr@gmail.com>
2020-03-23 19:26:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+newFunction();
+
+function newFunction() {
+    let name = "Ayuba";
+    console.log('My Name is'name);
+   
+    let courses = [html, css, js];
+    console.log(courses);
+    
+    let evenNumber = [];
+    for (let i = 0; i<= 200; i++) {
+        if (i % 2 === 0) {
+            evenNumber.push(i);
+        }
+    }
+    console.log(evenNumber);
